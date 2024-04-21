17afef272514f71ffc5e223a9d702572107c8e23
Hecdin <Hecdinnicola@outlook.com>
2024-04-21 08:47:26 -0400

    refactor: Rename variables for better consistency

--- lua/colorker/misc/config.lua
+++ lua/colorker/misc/config.lua
@@ -4,7 +4,7 @@ local M = {}
 M.options = {
   parent_search_limit = 5, -- <number>
   filename_to_track = "main", -- <string>
-  color_variable_pattern = "%-%-[-_%w]*color[-_%w]*", -- <string>
+  variable_pattern = "%-%-[-_%w]*color[-_%w]*", -- <string>
   disable_keymaps = false, -- <boolean>
 }
 
